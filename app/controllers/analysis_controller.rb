040a20324ee0f69a287edff6ea9d2dc0208a0c0c
Bartu Demirkiran <demirkiran.bartud@gmail.com>
2019-07-23 03:29:00 +0300

    add least and most used languages

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class AnalysisController < ApplicationController
-  def index; end
+  def index
+    service = Analysis::Db.new 'google'
+
+    @most_used_languages  = service.most_used_x_programming_languages 5
+    @least_used_languages = service.least_used_x_programming_languages 5
+  end
 end
